b1a63be3e9a29f43606f240a6b89caf1d9b30307
Carm01 <carmine900@gmail.com>
2019-02-03 21:40:25 -0500

    Update SetStaffStartMenu.cmd
    added a comment in line 14

--- SetStaffStartMenu.cmd
+++ SetStaffStartMenu.cmd
@@ -11,7 +11,7 @@ IF EXIST "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.
 ) ELSE (
   xcopy LayoutModification_Staff.xml C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification_Staff.xml* /y
 )
-
+:: This creates a shortcut for IE. For reasons I found that if i use the default in user profile the start menuxml is read before the shortcuts in the user profile are created upon first profile create
 xcopy "Internet Explorer.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Internet Explorer.lnk"* /y
 
 
